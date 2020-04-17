409e4d5506bc729c2c258a80b0528e4b98f482c8
CrabbleOrNiceTry <62458142+CrabbleOrNiceTry@users.noreply.github.com>
2020-04-16 22:57:25 -0600

    Update dayFinder.py

--- dayFinder.py
+++ dayFinder.py
@@ -2,9 +2,12 @@ months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 def useBigBrainToCalculateTheDay(year, month, day):
-
+    #IDK why I have to subtract two but I do, so Im not gonna question it haha. Tho it might be because of year 0001 and year 0000 if that existed.
     totalDaysPassed = (year - 2) * 365.2422222
+    if year % 4 == 0:
+        months[1] = 29
     for i in range(month):
+        
         totalDaysPassed += months[i]
     totalDaysPassed += day
     dayNumber = (int) (totalDaysPassed % 7)
